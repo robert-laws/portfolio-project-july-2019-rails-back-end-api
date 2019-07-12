11659e34b9726108720393eac30fc40e353ec441
Robert Laws <roblaws@me.com>
2019-07-12 18:12:17 +0300

    setup api with serializers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Book.create([{title: "Book of Fun", publication_year: 2015}, {title: "Places to See Today", publication_year: 2007}])
