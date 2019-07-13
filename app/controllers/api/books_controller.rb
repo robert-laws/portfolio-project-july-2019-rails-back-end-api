deffeda80ead176ec2a99f8bc1f9a5b3a96a2a64
Robert Laws <roblaws@me.com>
2019-07-13 06:08:45 +0300

    rails backend api

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -3,4 +3,19 @@ class Api::BooksController < ApplicationController
     @books = Book.all
     render json: @books
   end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      render json: @book
+    else
+      render json: { errors: { message: "This book failed to save" }}
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :publication_year)
+  end
 end
